afff730af9a8c6b54e132ea9f33ade044eb3f94a
TojoniainaSarobidy <hei.tojo.3@gmail.com>
2026-07-05 22:48:08 +0300

    chore: add mapper
    * feat: add entities
    
    * fix: entities
    
    * chore: add mapper
    
    ---------
    
    Co-authored-by: Tojoniaina Randrianasoa <tojoniaina@gmail.com>

---
+++ src/main/java/course/register/demo/mapper/CourseMapper.java
@@ -0,0 +1,41 @@
+package course.register.demo.mapper;
+
+import course.register.demo.model.Course;
+import course.register.demo.repository.model.JCourse;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@AllArgsConstructor
+@Component
+public class CourseMapper {
+  private final UserMapper userMapper;
+
+  public List<Course> courses(List<JCourse> jCourses) {
+    return jCourses.stream().map(this::toModel).toList();
+  }
+
+  public Course toModel(JCourse jCourse) {
+    return Course.builder()
+        .id(jCourse.getId())
+        .title(jCourse.getTitle())
+        .startDate(jCourse.getStartDate())
+        .endDate(jCourse.getEndDate())
+        .users(userMapper.toModel(jCourse.getUsers()))
+        .build();
+  }
+
+  public List<JCourse> toEntity(List<Course> courses) {
+    return courses.stream().map(this::toEntity).toList();
+  }
+
+  public JCourse toEntity(Course course) {
+    return JCourse.builder()
+        .id(course.id())
+        .title(course.title())
+        .startDate(course.startDate())
+        .endDate(course.endDate())
+        .users(userMapper.toEntity(course.users()))
+        .build();
+  }
+}
